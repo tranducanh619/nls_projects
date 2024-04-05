acbff6f2e441e07682a2be103e0173c074f579c2
Tran Duc Anh <tranducanh619@gmail.com>
2024-04-05 22:06:11 +0700

    Them trang chu

---
+++ resources/views/trangchu.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Website Ôn tập môn năng lực số</title>
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="../css/app.css">
+</head>
+<body>
+<!-- Header -->
+<nav class="custom-navbar">
+  <a href="#" class="navbar-brand">NĂNG LỰC SỐ</a>
+  <ul class="navbar-list">
+    <li><a href="#" class="navbar-item">Giới thiệu</a></li>
+    <li><a href="#" class="navbar-item">Ôn tập</a></li>
+    <li><a href="#" class="navbar-item">Diễn đàn</a></li>
+    <li><a href="#" class="navbar-item">Liên hệ</a></li>
+  </ul>
+</nav>
+<header class="w3-container w3-theme w3-padding" id="myHeader">
+
+  
+  <div class="w3-center">
+  <h4>Học viện Ngân hàng</h4>
+  <h1 class="w3-xxxlarge w3-animate-bottom">Năng lực số ứng dụng</h1>
+    <div class="w3-padding-32">
+      <button class="w3-btn w3-xlarge w3-dark-grey w3-hover-light-grey" onclick="document.getElementById('forum').style.display='block'" style="font-weight:900;">Tham Gia Ngay</button>
+    </div>
+  </div>
+  
+</header>
+
+
+<!-- Modal -->
+
+<div class="w3-row-padding w3-center w3-margin-top" id = "id0101">
+<div class="w3-third">
+  <div class="w3-card w3-container" style="min-height:460px">
+  <h3>Responsive</h3><br>
+  <i class="fa fa-desktop w3-margin-bottom w3-text-theme" style="font-size:120px"></i>
+  <p>Built-in responsiveness</p>
+  <p>Mobile first fluid grid</p>
+  <p>Fits any screen sizes</p>
+  <p>PC Tablet and Mobile</p>
+  </div>
+</div>
+
+<div class="w3-third">
+  <div class="w3-card w3-container" style="min-height:460px">
+  <h3>Standard CSS</h3><br>
+  <i class="fa fa-css3 w3-margin-bottom w3-text-theme" style="font-size:120px"></i>
+  <p>Standard CSS only</p>
+  <p>Easy to learn</p>
+  <p>No need for jQuery</p>
+  <p>No JavaScript library</p>
+  </div>
+</div>
+
+<div class="w3-third">
+  <div class="w3-card w3-container" style="min-height:460px">
+  <h3>Design</h3><br>
+  <i class="fa fa-diamond w3-margin-bottom w3-text-theme" style="font-size:120px"></i>
+  <p>Paper like design</p>
+  <p>Bold colors and shadows</p>
+  <p>Equal across platforms</p>
+  <p>Equal across devices</p>
+  </div>
+</div>
+</div>
+<script src="../js/function.js"></script>
+<!-- Footer -->
+<footer class="w3-container w3-theme-dark w3-padding-16">
+  <h3>Footer</h3>
+  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+  <div style="position:relative;bottom:55px;" class="w3-tooltip w3-right">
+    <span class="w3-text w3-theme-light w3-padding">Go To Top</span>    
+    <a class="w3-text-white" href="#myHeader"><span class="w3-xlarge">
+    <i class="fa fa-chevron-circle-up"></i></span></a>
+  </div>
+  <p>Remember to check out our  <a href="w3css_references.asp" class="w3-btn w3-theme-light" target="_blank">W3.CSS Reference</a></p>
+</footer>
+
+</body>
+</html>
